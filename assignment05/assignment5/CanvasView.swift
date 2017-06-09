d45e0590ce9782327e8a7f6210800a308abaf8b7
ginpei <ginpei@gmail.com>
2017-06-09 02:45:22 -0700

    use common properties

--- assignment05/assignment5/CanvasView.swift
+++ assignment05/assignment5/CanvasView.swift
@@ -182,20 +182,18 @@ class CanvasView: UIView, UITextFieldDelegate {
     class Text: Content {
         let string: String
         let frame: CGRect
-        let attr: [String: Any]
         
         init(_ string: String, at frame: CGRect, color: CGColor, size: Double) {
             self.string = string
             self.frame = frame
-            self.attr = [
-                "NSForegroundColorAttributeName": UIColor(cgColor: color),
-                "NSFontAttributeName": UIFont.systemFont(ofSize: CGFloat(size)),
-            ]
-            
             super.init(color: color, size: size)
         }
         
         override func draw(on context: CGContext) {
+            let attr = [
+                "NSForegroundColorAttributeName": UIColor(cgColor: color),
+                "NSFontAttributeName": UIFont.systemFont(ofSize: CGFloat(strokeWidth)),
+                ]
             string.draw(in: frame, withAttributes: attr)
         }
     }
