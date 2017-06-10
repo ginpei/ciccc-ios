d725ec098af1af03d6b7951e2fd107af87b4e658
ginpei <ginpei@gmail.com>
2017-06-09 18:42:38 -0700

    set custom view

---
+++ assignment07-MyScrollView/assignment07-MyScrollView/MyScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  MyScrollView.swift
+//  assignment07-MyScrollView
+//
+//  Created by Ginpei on 2017-06-09.
+//  Copyright © 2017 Ginpei. All rights reserved.
+//
+
+import UIKit
+
+class MyScrollView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
