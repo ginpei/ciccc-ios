cb1021c1040e5222af69f2ab409b1818f4b0f265
ginpei <ginpei@gmail.com>
2017-06-14 15:33:28 -0700

    constraints

--- assignment08-ImageGallery/assignment08-ImageGallery/ViewController.swift
+++ assignment08-ImageGallery/assignment08-ImageGallery/ViewController.swift
@@ -16,10 +16,15 @@ class ViewController: UIViewController {
         
         let imageView = UIImageView()
         imageView.backgroundColor = UIColor.red
-        imageView.frame.origin.y = 500
-        imageView.frame.size.width = 300.0
-        imageView.frame.size.height = 300.0
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
         scrollView.addSubview(imageView)
+        
+        imageView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 0).isActive = true
+        imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1).isActive = true
+        
+        imageView.leftAnchor.constraint(equalTo: scrollView.leftAnchor, constant: 0).isActive = true
+        imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
