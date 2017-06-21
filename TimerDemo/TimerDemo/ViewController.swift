8c215b79326347285d7e18445dfdcd63af4d24b7
ginpei <ginpei@gmail.com>
2017-06-21 11:23:44 -0700

    count up

--- TimerDemo/TimerDemo/ViewController.swift
+++ TimerDemo/TimerDemo/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var timer = Timer()
+    var startedAt: Date? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func start() {
+        startedAt = Date()
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timer_tick), userInfo: nil, repeats: true)
+    }
 
-
+    @IBAction func startButton_touchUpInside(_ sender: Any) {
+        start()
+    }
+    
+    func timer_tick() {
+        if let s = startedAt {
+            let intervalSince = Date().timeIntervalSince(s)
+            let text = String(format: "%.02f", intervalSince)
+            timeLabel.text = text
+        }
+    }
 }
 
