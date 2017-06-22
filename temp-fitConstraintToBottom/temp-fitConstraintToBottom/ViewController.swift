6b45fe39402462d731f35fbbb5c9482d6280f9d1
ginpei <ginpei@gmail.com>
2017-06-22 15:20:03 -0700

    show a view at half bottom

--- temp-fitConstraintToBottom/temp-fitConstraintToBottom/ViewController.swift
+++ temp-fitConstraintToBottom/temp-fitConstraintToBottom/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var childView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let v = UIView()
+        v.backgroundColor = UIColor.red
+        v.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(v)
+        
+        v.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height / 2).isActive = true
+//        v.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        v.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        v.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        v.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        v.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
