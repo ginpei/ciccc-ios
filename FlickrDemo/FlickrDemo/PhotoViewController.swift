e46514620ba7094536c974320af543dbc4132d0d
ginpei <ginpei@gmail.com>
2017-07-08 12:51:41 -0700

    remove commented out codes
    Guess not necessary now?

--- FlickrDemo/FlickrDemo/PhotoViewController.swift
+++ FlickrDemo/FlickrDemo/PhotoViewController.swift
@@ -91,17 +91,6 @@ class PhotoViewController: UIViewController, UICollectionViewDataSource, UIColle
         self.photoImages.removeAll()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return photos.count
     }
