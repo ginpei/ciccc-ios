c0c89c356bfabe095c35d06f215f547f3afbdd2d
ginpei <ginpei@gmail.com>
2017-07-08 12:50:56 -0700

    clear cache when received memory warning

--- FlickrDemo/FlickrDemo/PhotoViewController.swift
+++ FlickrDemo/FlickrDemo/PhotoViewController.swift
@@ -87,7 +87,8 @@ class PhotoViewController: UIViewController, UICollectionViewDataSource, UIColle
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        self.photoImages.removeAll()
     }
     
 
