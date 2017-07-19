7c79e9b00f3c6d3f6f94e4f4eaee993c35d14e32
ginpei <ginpei@gmail.com>
2017-07-19 15:55:35 -0700

    arrange items in a collection

---
+++ WelcomePageControlDemo/WelcomePageControlDemo/PageCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PageCollectionViewCell.swift
+//  WelcomePageControlDemo
+//
+//  Created by Ginpei on 2017-07-18.
+//  Copyright © 2017 Ginpei. All rights reserved.
+//
+
+import UIKit
+
+class PageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    static let identifier = "PageCollectionViewCell"
+    
+}
--- WelcomePageControlDemo/WelcomePageControlDemo/ViewController.swift
+++ WelcomePageControlDemo/WelcomePageControlDemo/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pages = [
+        Page(title: "Ichi"),
+        Page(title: "Ni"),
+        Page(title: "San"),
+        ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+class Page {
+    var title: String!
+    
+    init(title: String) {
+        self.title = title
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let page = pages[indexPath.row]
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PageCollectionViewCell.identifier, for: indexPath) as! PageCollectionViewCell
+        cell.titleLabel.text = page.title
+        
+        return cell
+    }
+}
